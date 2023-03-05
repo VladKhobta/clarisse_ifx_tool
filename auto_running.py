76c63ba5d4946e405a37a6513dd6dd74b6403ade
Vlad <vlad.hobta79@mail.ru>
2023-03-05 13:41:52 +0300

    screenshots added

--- auto_running.py
+++ auto_running.py
@@ -1,5 +1,6 @@
 # module for running by batch-script
 import os
+import sys
 
 
 # parent dir for all clarisse projects
@@ -10,6 +11,7 @@ def saving_project(app: ix.api.ClarisseApp, file_path: str) -> bool:
     return app.save_project(file_path)
 
 
+# universal function for adding any scripts to the 'custom' shelf
 def add_to_shelf(app, path: str,  name: str = '', description: str = ''):
     shelf = app.get_shelf()
     shelf.add_item(0, 'custom', name, description, path, )
@@ -23,6 +25,9 @@ if __name__ == "__main__":
     if not os.path.exists(full_path):
         os.makedirs(full_path)
 
-    add_to_shelf(clarisse_app, r".\reference_script.py", "making reference to the picked object")
+    # checking if command line arguments from batch-script have passed and getting current dir with ref script
+    if len(sys.argv) > 1:
+        current_dir = sys.argv[1]
+        add_to_shelf(clarisse_app, current_dir + r"\reference_script.py", "making reference to the picked object")
 
     saving_project(clarisse_app, full_path + r"\bui_a_geo_v001.project")
